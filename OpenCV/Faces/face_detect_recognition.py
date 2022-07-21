5a253e52f66fd629335d5944c7e35e145c63f64e
Ravinder <79361243+Dixit01@users.noreply.github.com>
2022-07-21 23:33:32 +0530

    Create face_detect_recognition.py

---
+++ OpenCV/Faces/face_detect_recognition.py
@@ -0,0 +1,107 @@
+#Face_Train
+
+import os
+import cv2 as cv
+import numpy as np
+
+people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
+DIR = r'..Media Files\Faces\train'
+
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
+
+features = []
+labels = []
+
+def create_train():
+    for person in people:
+        path = os.path.join(DIR, person)
+        label = people.index(person)
+
+        for img in os.listdir(path):
+            img_path = os.path.join(path,img)
+
+            img_array = cv.imread(img_path)
+            if img_array is None:
+                continue 
+                
+            gray = cv.cvtColor(img_array, cv.COLOR_BGR2GRAY)
+
+            faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
+
+            for (x,y,w,h) in faces_rect:
+                faces_roi = gray[y:y+h, x:x+w]
+                features.append(faces_roi)
+                labels.append(label)
+
+create_train()
+print('Training done ---------------')
+
+features = np.array(features, dtype='object')
+labels = np.array(labels)
+
+face_recognizer = cv.face.LBPHFaceRecognizer_create()
+
+# Train the Recognizer on the features list and the labels list
+face_recognizer.train(features,labels)
+
+face_recognizer.save('face_trained.yml')
+np.save('features.npy', features)
+np.save('labels.npy', labels)
+
+
+
+# Face_Detection
+
+img = cv.imread('../Resources/Photos/group 1.jpg')
+cv.imshow('Group of 5 people', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray People', gray)
+
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
+
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1)
+
+print(f'Number of faces found = {len(faces_rect)}')
+
+for (x,y,w,h) in faces_rect:
+    cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=2)
+
+cv.imshow('Detected Faces', img)
+cv.waitKey(0)
+
+
+
+#Face_recognition
+
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
+
+people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
+# features = np.load('features.npy', allow_pickle=True)
+# labels = np.load('labels.npy')
+
+face_recognizer = cv.face.LBPHFaceRecognizer_create()
+face_recognizer.read('face_trained.yml')
+
+img = cv.imread(r'../Resources\Faces\val\elton_john/1.jpg')
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Person', gray)
+
+# Detect the face in the image
+faces_rect = haar_cascade.detectMultiScale(gray, 1.1, 4)
+
+for (x,y,w,h) in faces_rect:
+    faces_roi = gray[y:y+h,x:x+w]
+
+    label, confidence = face_recognizer.predict(faces_roi)
+    print(f'Label = {people[label]} with a confidence of {confidence}')
+
+    cv.putText(img, str(people[label]), (20,20), cv.FONT_HERSHEY_COMPLEX, 1.0, (0,255,0), thickness=2)
+    cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=2)
+
+cv.imshow('Detected Face', img)
+
+cv.waitKey(0)
+
+
