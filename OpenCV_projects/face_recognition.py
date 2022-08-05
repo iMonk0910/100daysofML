a8c04547c3ae3983f39f5803f1180564bbe7a54b
Ravinder <79361243+Dixit01@users.noreply.github.com>
2022-08-06 01:43:56 +0530

    Update face_recognition.py

--- OpenCV_projects/face_recognition.py
+++ OpenCV_projects/face_recognition.py
@@ -1,12 +1,12 @@
 import cv2, numpy, os #OpenCV library
 size = 4
-model = "haarcascade_frontalface_default.xml" #accessed the model file
-haar_cascade = cv2.CascadeClassifier(model) #loading the model with cv2
-datasets = "datasets"
-(image,labels, names, id) = ([], [], {}, 0)
-for (subdirs, dirs, files) in os.walk(detasets):
+haar_file = "haarcascade_frontalface_default.xml" #accessed the model file
+datasets = "/content/datasets"
+(images,labels, names, id) = ([], [], {}, 0)
+for (subdirs, dirs, files) in os.walk(datasets):
   for subdir in dir:
     names[id] = subdir
+
     subjectpath = os.path.join(datasets, subdir)
     for filename in os.listdir(subjectpath):
       path = subjectpath + "/" + filename
@@ -28,4 +28,28 @@ while True:
     _,Img = Camera.read()      #reading the frame from the video
    ## Img = imutils.resize(Img, width=500) #resize
     grayImg = cv2.cvtColor(Img, cv2.COLOR_BGR2GRAY) #color to gray scale image
-    face = haar_cascade.detectMultiScale(grayImg,1.3, 4) #gets cordinates of the face
+    faces = haar_cascade.detectMultiScale(grayImg,1.3, 4) #gets cordinates of the face
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(Img, (x, y), (x+w, y+h), (255, 255, 0),2)
+        face = gray[y:y + h, x:x + w]
+        face_resize = cv2.resize(face,(width, height))
+        prediction = model.predict(face_resize)
+        cv2.rectangle(Img, (x, y), (x+w, y+h), (0, 255, 0),3)
+        if prediction[1]<800:
+          cv2.putText(Img, "%s - %.0f" % (names[prediction[0]],prediction[1]),(x-10, y-20))
+          print(names[prediction[0]])
+          cnt = 0
+        else:
+          cnt+=1
+          cv2.putText(Img,'Unknown'(x-10, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1,(0, 255, 0), 2)      
+          if(cnt>100):
+            print("Unknown Person")
+            cv2.imwrite("input.jpg",Img)
+            cnt = 0
+    cv2.imshow("OpenCV", Img)
+    key = cv2.waitKey(10 & 0xFF)
+    if key == ord("q"):
+      break
+Camera.release() # releasing the camera
+cv2.destroyAllWindows() #destroy all windows
